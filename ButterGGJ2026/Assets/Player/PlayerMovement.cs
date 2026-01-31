57aa8328f76438275cf1494f1f7762ff6c0e6ec3
Reece Roberts <reece.roberts288@gmail.com>
2026-01-31 01:14:41 +0000

    PlayerMovement V0.9
    Player can now jump
    
    But pressing the button repeatedly will cause them to fly forever

--- ButterGGJ2026/Assets/Player/PlayerMovement.cs
+++ ButterGGJ2026/Assets/Player/PlayerMovement.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2d;
 
     private float moveSpeed = 5.0f;
+
+    private float jumpSpeed = 5.0f;
 
     private Vector2 moveInput;
 
@@ -18,23 +22,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb2d.linearVelocity = moveInput * moveSpeed;
+        rb2d.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb2d.linearVelocity.y);
     }
 
     //Whenever the player calls this input, they'll move left to right
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move triggered");
     }
 
     //Whenever the player calls this input, the player will jump up
     public void Jump(InputAction.CallbackContext context)
     {
-
+        if (context.performed)
+        {
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpSpeed);
+        }
+        Debug.Log("Jump triggered");
     }
 
     public void Interact(InputAction.CallbackContext context)
     {
-        
+        Debug.Log("Interact triggered");
     }
 }
