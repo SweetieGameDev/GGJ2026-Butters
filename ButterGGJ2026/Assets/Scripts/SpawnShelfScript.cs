1529c8b56d6170d05e092af95665491db7229f81
CubeSteve <oliver.chalkmsxb@outlook.com>
2026-02-01 15:46:59 +0000

    Level 3 fixes

--- ButterGGJ2026/Assets/Scripts/SpawnShelfScript.cs
+++ ButterGGJ2026/Assets/Scripts/SpawnShelfScript.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class SpawnShelfScript : MonoBehaviour
 {
     public GameObject shelfPrefab;
+    private bool shelfSpawned = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.ToString() == "Player")
+        if (collision.gameObject.tag.ToString() == "Player" && !shelfSpawned)
         {
+            shelfSpawned = true;
             Instantiate(shelfPrefab);
+            Destroy(this.gameObject);
         }
     }
 }
