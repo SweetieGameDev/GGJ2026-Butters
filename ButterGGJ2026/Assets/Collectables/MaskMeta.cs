d0271ad452a366271cb0090b4c4d564b8d00c232
Reece Roberts <reece.roberts288@gmail.com>
2026-02-01 10:28:56 +0000

    Sound V1.2
    Implemented sound effect and animation controller for collectable mask

--- ButterGGJ2026/Assets/Collectables/MaskMeta.cs
+++ ButterGGJ2026/Assets/Collectables/MaskMeta.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,16 +7,50 @@ public class MaskMeta : MonoBehaviour
 {
     public int MasksCollected;
 
+    private Animator maskAnimator;
+
+    private AudioSource maskBreak;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MasksCollected = 0;
+
+        maskAnimator = GetComponent<Animator>();
+
+        maskBreak = GetComponent<AudioSource>();
+
+        maskAnimator.SetBool("isCollected", false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         MasksCollected += 1;
+
+        StartCoroutine(WaitForAnimation());
     }
+
+    private IEnumerator WaitForAnimation()
+    {
+        maskAnimator.SetBool("isCollected", true);
+
+        //Play mask breaking sound effect
+        maskBreak.Play();
+
+        // Wait for one frame to ensure that the animation has started
+        yield return null;
+
+        // Get the length of the current animation, which will be "isDeath"
+        float animationLength = maskAnimator.GetCurrentAnimatorStateInfo(0).length;
+
+        // Wait for the duration of the enemy death animation
+        yield return new WaitForSeconds(animationLength);
+
+        //Animation is done destory self
+        StopCoroutine(WaitForAnimation());
+        Destroy(gameObject);
+    }
+
 
     public void TriggerEnding()
     {
